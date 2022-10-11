99fe7e9e304a755301af4a66b6f14e8ea0a45a7e
Eros Linnyker <linnykereros@gmail.com>
2022-10-11 17:15:46 -0300

    added and fix: Logo and eslint

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
 import History from "./pages/History";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
-      <Route />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+      {/* <Route path="/admin" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route> */}
     </Routes>
   );
 }
